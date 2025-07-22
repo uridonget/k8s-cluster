b4b0f07cb8a332a241859c44a94ed8b4b73523d6
uridonget <14010008@naver.com>
2025-07-22 14:50:19 +0900

    make-vms fixed

--- make-vms.sh
+++ make-vms.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 DEFAULT_NODES=3
-MAX_NODES=10
+MAX_NODES=5
 N=${1:-$DEFAULT_NODES}
 
-if ! [[ "$N" =~ ^[0-9]+$ ]] || [ "$N" -le 0 ] || [ "$N" -gt "$MAX_NODES" ]; then
+if ! [[ "$N" =~ ^[0-9]+$ ]] || [ "$N" -lt 3 ] || [ "$N" -gt "$MAX_NODES" ]; then
   echo "Error: Invalid number of nodes."
-  echo "Please provide a number between 1 and $MAX_NODES."
+  echo "Please provide a number between 3 and $MAX_NODES."
   exit 1
 fi
 
